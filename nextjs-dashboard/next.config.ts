8724490b1eeb26008268de6797b76ed7c6036733
g g <gabedotgutierrez@gmail.com>
2025-03-21 13:54:15 -0400

    added pagnation

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: { //literally experemental still in 2025, chap 10.
+    ppr: 'incremental'
+  }
 };
 
 export default nextConfig;
